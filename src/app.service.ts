fca545eff294b20fbd60eec1e8d43fbf90db40e0
Tiz2930 <tizianoschwindt3@gmail.com>
2025-06-03 00:03:51 -0300

    Ej audios

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,66 @@
 import { Injectable } from '@nestjs/common';
 
+export interface iTrack {
+  id: number,
+  title: string,
+  duration: number,
+  artist: string
+}
+
+export const tracks: iTrack[] = [
+  {
+    id: 1,
+    title: "cancion 1",
+    duration: 120,
+    artist: "interprete 1"
+  },
+  {
+    id: 1,
+    title: "cancion 1",
+    duration: 120,
+    artist: "interprete 1"
+  },
+  {
+    id: 1,
+    title: "cancion 1",
+    duration: 120,
+    artist: "interprete 1"
+  },
+  {
+    id: 1,
+    title: "cancion 1",
+    duration: 120,
+    artist: "interprete 1"
+  },
+  {
+    id: 1,
+    title: "cancion 1",
+    duration: 120,
+    artist: "interprete 1"
+  },
+  {
+    id: 1,
+    title: "cancion 1",
+    duration: 120,
+    artist: "interprete 1"
+  },
+  {
+    id: 1,
+    title: "cancion 1",
+    duration: 120,
+    artist: "interprete 1"
+  },
+  {
+    id: 1,
+    title: "cancion 1",
+    duration: 120,
+    artist: "interprete 1"
+  }
+]
+
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getTracks(): iTrack[] {
+    return tracks;
   }
 }
